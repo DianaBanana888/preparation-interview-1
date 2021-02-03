e8580b3a74738cc309091a30e4006c81229c0b85
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-02-03 14:10:05 +0100

    создала модель таймер

---
+++ public/js/Timer.js
@@ -0,0 +1,37 @@
+// класс для создания объекта таймер
+// при создании экземпляра передаем круглое число минут, минимум одна минута
+// при вызове метода start(display) передаем аргументом элемент на странице
+
+class Timer {
+  constructor(startMinutes) {
+    this.timeLeft = startMinutes * 60;
+
+    this.minutes = startMinutes;
+    this.seconds = '00';
+
+    this.on = false;
+  }
+
+  start(display) {
+    this.on = true;
+
+    const countDown = setInterval(() => {
+      if (this.timeLeft <= 0) {
+        clearInterval(countDown);
+        this.stop();
+      }
+
+      this.minutes = Math.floor(this.timeLeft / 60);
+      this.seconds = this.timeLeft % 60;
+      this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+      this.timeLeft -= 1;
+
+      // вывод времени
+      display.innerText = `${timer.minutes}:${timer.seconds}`;
+    }, 1000);
+  }
+
+  stop() {
+    this.on = false;
+  }
+}
